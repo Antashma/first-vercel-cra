efac347be76739040cd4b56b7c86992bfb033a2c
Trevor Alexander Jefferson <coinoperatedguy4@gmail.com>
2020-10-07 23:19:01 -0400

    ME

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
   return (
     <main>
       <h1>This is Sam G&apos;s priject page</h1>
+      <div>
+        <h2>Im Trevor!</h2>
+        <img src='src\picture\puppy-work.jpg'/>
+      </div>
     </main>
   );
 }
